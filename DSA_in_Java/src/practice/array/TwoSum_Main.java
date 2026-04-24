e50e791be54828e2d880c6b754328684e311ab72
Gsahu46 <gsahu4900@gmail.com>
2026-04-24 08:40:51 +0530

    Two Sum problem in Bruteforce way.

---
+++ DSA_in_Java/src/practice/array/TwoSum_Main.java
@@ -0,0 +1,34 @@
+package practice.array;
+
+class TwoSum{
+	
+	static boolean twoSumProblem(int[] arr, int target) {
+		
+		int n = arr.length;
+		
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = i+1; j < n; j++) {
+				if(arr[i] + arr[j] == target) {
+					System.out.println("Yes target is found: " + arr[j]);
+					return true;
+				}
+			}
+		}
+		return false;
+		
+	}
+}
+
+public class TwoSum_Main {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {0, -1, 6 , 5, -2};
+		int target = -2;
+		
+		TwoSum n1 = new TwoSum();
+		n1.twoSumProblem(arr, target);
+	}
+
+}
